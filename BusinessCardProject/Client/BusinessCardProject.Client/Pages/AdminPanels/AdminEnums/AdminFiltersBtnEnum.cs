56bbd56886889ce7c1293a6a48fad70d4d49f9bf
Nikita Skibko <skibko.nv@mmk.ru>
2025-10-26 01:57:41 +0500

    Разработка админ-панели, fix Nav-панели, fix icons, доработки по существующим страницам

---
+++ BusinessCardProject/Client/BusinessCardProject.Client/Pages/AdminPanels/AdminEnums/AdminFiltersBtnEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessCardProject.Client.Pages.AdminPanels.AdminEnums
+{
+    /// <summary>
+    /// Справочник кнопок в админ-панели
+    /// </summary>
+    public enum AdminFiltersBtnEnum
+    {
+        Graphs,
+        Servers,
+        Orders,
+        Users,
+        Audits
+    }
+}
